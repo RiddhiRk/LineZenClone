fad53c14187838092e9a1074db265c141a104f1b
RiddhiRk <Rkkothari106@gmail.com>
2017-05-17 09:17:49 +0530

    Add files via upload

---
+++ Scripts/DataManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DataManager : MonoBehaviour {
+
+#region Public_Variables
+    public List<Sprite> listOfDifferentShapes=new List<Sprite>(); 
+#endregion
+
+#region Private_Variables
+#endregion
+
+#region Events
+#endregion
+
+	#region Unity_CallBacks
+	void Awake(){
+	
+	}
+
+	void OnEnable(){
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDisable(){
+	
+	}
+	#endregion
+
+#region Private_Methods
+#endregion
+
+#region Public_Methods
+#endregion
+
+#region Coroutines
+#endregion
+
+#region Custom_CallBacks
+#endregion
+}
---
+++ Scripts/ObjectPoolingDemo.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPoolingDemo : MonoBehaviour
+{
+    #region Public_Variables
+    public GameObject obj;
+    public static ObjectPoolingDemo instance; //Singleton
+    public int pooledAmount;
+    public bool isPoolGrow;
+    public int maxGrowAmount;
+    public List<GameObject> pooledObjectList;
+    #endregion
+
+    #region Private_Variables
+    private GameObject _tempGameObject;
+    private int _count = 0;
+    #endregion
+
+    #region Events
+    #endregion
+
+    #region Unity_CallBacks
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void OnEnable()
+    {
+        
+    }
+
+    void OnDisable()
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        pooledObjectList = new List<GameObject>();
+        for (int i = 0; i < pooledAmount; i++)
+        {
+            _tempGameObject = (GameObject)Instantiate(obj);      
+            _tempGameObject.SetActive(false);
+            pooledObjectList.Add(_tempGameObject);
+        }
+        //PathManager.instance.InitialCenterPoints();
+    }
+
+    #endregion
+
+    #region Private_Methods
+    #endregion
+
+    #region Public_Methods
+  
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < pooledObjectList.Count; i++)
+        {
+            if (!pooledObjectList[i].activeInHierarchy)
+            {
+                return pooledObjectList[i];
+            }
+        }
+
+        if (isPoolGrow)// && pooledObjectList.Count < maxGrowAmount)
+        {
+            _tempGameObject = (GameObject)Instantiate(obj);
+            pooledObjectList.Add(_tempGameObject);
+            return _tempGameObject;
+        }
+        return null;
+    }
+
+    public void DestroyPooledObject(GameObject go)
+    {
+        go.SetActive(false);
+    }
+
+    public void GameOverDisableObjects()
+    {
+        //pooledObjectList.Clear();
+    }
+
+    public void ResetPositions()
+    {
+        _count = 0;
+    }
+
+    #endregion
+
+    #region Coroutines
+    #endregion
+
+    #region Custom_CallBacks
+    #endregion
+}
